ec2b9b3183a386e79088d5b7be4ca1132a943b6f
Preston Wang-Stosur-Bassett <p.wanstobas@gmail.com>
2022-08-02 01:51:57 -0400

    Minor updates

--- src/lib.rs
+++ src/lib.rs
@@ -47,30 +47,132 @@ mod tests {
 /// ```
 pub fn prettify(raw: String) -> String {
     let mut replacements: HashMap<String, Vec<String>> = HashMap::new();
-    replacements.insert(String::from("a"), vec![String::from("ā"), String::from("á"), String::from("ǎ"), String::from("à")]);
-    replacements.insert(String::from("e"), vec![String::from("ē"), String::from("é"), String::from("ě"), String::from("è")]);
-    replacements.insert(String::from("u"), vec![String::from("ū"), String::from("ú"), String::from("ǔ"), String::from("ù")]);
-    replacements.insert(String::from("i"), vec![String::from("ī"), String::from("í"), String::from("ǐ"), String::from("ì")]);
-    replacements.insert(String::from("o"), vec![String::from("ō"), String::from("ó"), String::from("ǒ"), String::from("ò")]);
-    replacements.insert(String::from("ü"), vec![String::from("ǖ"), String::from("ǘ"), String::from("ǚ"), String::from("ǜ")]);
-    replacements.insert(String::from("A"), vec![String::from("Ā"), String::from("Á"), String::from("Ă"), String::from("À")]);
-    replacements.insert(String::from("E"), vec![String::from("Ē"), String::from("É"), String::from("Ĕ"), String::from("È")]);
-    replacements.insert(String::from("U"), vec![String::from("Ū"), String::from("Ú"), String::from("Ŭ"), String::from("Ù")]);
-    replacements.insert(String::from("I"), vec![String::from("Ī"), String::from("Í"), String::from("Ĭ"), String::from("Ì")]);
-    replacements.insert(String::from("O"), vec![String::from("Ō"), String::from("Ó"), String::from("Ŏ"), String::from("Ò")]);
-    replacements.insert(String::from("Ü"), vec![String::from("Ǖ"), String::from("Ǘ"), String::from("Ǚ"), String::from("Ǜ")]);
+    replacements.insert(
+        String::from("a"),
+        vec![
+            String::from("ā"),
+            String::from("á"),
+            String::from("ǎ"),
+            String::from("à"),
+        ],
+    );
+    replacements.insert(
+        String::from("e"),
+        vec![
+            String::from("ē"),
+            String::from("é"),
+            String::from("ě"),
+            String::from("è"),
+        ],
+    );
+    replacements.insert(
+        String::from("u"),
+        vec![
+            String::from("ū"),
+            String::from("ú"),
+            String::from("ǔ"),
+            String::from("ù"),
+        ],
+    );
+    replacements.insert(
+        String::from("i"),
+        vec![
+            String::from("ī"),
+            String::from("í"),
+            String::from("ǐ"),
+            String::from("ì"),
+        ],
+    );
+    replacements.insert(
+        String::from("o"),
+        vec![
+            String::from("ō"),
+            String::from("ó"),
+            String::from("ǒ"),
+            String::from("ò"),
+        ],
+    );
+    replacements.insert(
+        String::from("ü"),
+        vec![
+            String::from("ǖ"),
+            String::from("ǘ"),
+            String::from("ǚ"),
+            String::from("ǜ"),
+        ],
+    );
+    replacements.insert(
+        String::from("A"),
+        vec![
+            String::from("Ā"),
+            String::from("Á"),
+            String::from("Ă"),
+            String::from("À"),
+        ],
+    );
+    replacements.insert(
+        String::from("E"),
+        vec![
+            String::from("Ē"),
+            String::from("É"),
+            String::from("Ĕ"),
+            String::from("È"),
+        ],
+    );
+    replacements.insert(
+        String::from("U"),
+        vec![
+            String::from("Ū"),
+            String::from("Ú"),
+            String::from("Ŭ"),
+            String::from("Ù"),
+        ],
+    );
+    replacements.insert(
+        String::from("I"),
+        vec![
+            String::from("Ī"),
+            String::from("Í"),
+            String::from("Ĭ"),
+            String::from("Ì"),
+        ],
+    );
+    replacements.insert(
+        String::from("O"),
+        vec![
+            String::from("Ō"),
+            String::from("Ó"),
+            String::from("Ŏ"),
+            String::from("Ò"),
+        ],
+    );
+    replacements.insert(
+        String::from("Ü"),
+        vec![
+            String::from("Ǖ"),
+            String::from("Ǘ"),
+            String::from("Ǚ"),
+            String::from("Ǜ"),
+        ],
+    );
 
     let mut medials: HashMap<String, u8> = HashMap::new();
-    medials.insert(String::from("i"), 0 as u8);
-    medials.insert(String::from("u"), 0 as u8);
-    medials.insert(String::from("ü"), 0 as u8);
+    medials.insert(String::from("i"), 0_u8);
+    medials.insert(String::from("u"), 0_u8);
+    medials.insert(String::from("ü"), 0_u8);
 
     let mut syl_vec: Vec<String> = Vec::new();
-    let text = raw.replace("v", "ü");
-    let syllables: Vec<_> = text.split(" ").collect();
+    let text = raw.replace('v', "ü");
+    let syllables: Vec<_> = text.split(' ').collect();
 
     for syllable in syllables {
-        let tone: u8 = match syllable.chars().skip(syllable.chars().count() - 1).take(1).collect::<String>().parse::<u8>() {
+        let tone: u8 = match syllable
+            .chars()
+            .skip(syllable.chars().count() - 1)
+            .take(1)
+            .collect::<String>()
+            .parse::<u8>()
+        {
             Ok(tone) => tone,
             Err(_e) => {
                 syl_vec.push(syllable.to_string());
@@ -78,36 +180,53 @@ pub fn prettify(raw: String) -> String {
             }
         };
 
-        if tone <= 0 as u8 || tone > 5 as u8 {
+        if tone == 0_u8 || tone > 5_u8 {
             // This is not a valid number
             println!("Invalid tone number: {:?} in: {:?}", tone, syllable);
-        } else if tone == 5 as u8 {
-            let pretty_syl: String = syllable.chars().take(syllable.chars().count() - 1).collect();
+        } else if tone == 5_u8 {
+            let pretty_syl: String = syllable
+                .chars()
+                .take(syllable.chars().count() - 1)
+                .collect();
             syl_vec.push(pretty_syl);
         } else {
-            let mut j = 0 as u8;
+            let mut j = 0_u8;
             let mut done = false;
 
             while !done {
                 let current_letter: String = syllable.chars().skip(j as usize).take(1).collect();
-                let next_letter: String = syllable.chars().skip((j + (1 as u8)) as usize).take(1).collect();
+                let next_letter: String = syllable
+                    .chars()
+                    .skip((j + (1_u8)) as usize)
+                    .take(1)
+                    .collect();
 
                 if replacements.contains_key(&current_letter) {
-                    let to_replace;
-
-                    if replacements.contains_key(&next_letter) && medials.contains_key(&current_letter) {
-                        to_replace = next_letter;
+                    let to_replace = if replacements.contains_key(&next_letter)
+                        && medials.contains_key(&current_letter)
+                    {
+                        next_letter
                     } else {
-                        to_replace = current_letter;
-                    }
+                        current_letter
+                    };
 
-                    let replaced: String = syllable.replace(&to_replace, replacements.get(&to_replace).unwrap().get((tone - (1 as u8)) as usize).unwrap());
-                    let pretty_syl: String = replaced.chars().take(replaced.chars().count() - 1).collect();
+                    let replaced: String = syllable.replace(
+                        &to_replace,
+                        replacements
+                            .get(&to_replace)
+                            .unwrap()
+                            .get((tone - (1_u8)) as usize)
+                            .unwrap(),
+                    );
+                    let pretty_syl: String = replaced
+                        .chars()
+                        .take(replaced.chars().count() - 1)
+                        .collect();
                     syl_vec.push(pretty_syl);
                     break;
                 }
 
-                j = j + (1 as u8);
+                j += 1_u8;
                 if j as usize == syllable.chars().count() {
                     done = true;
                 }
@@ -116,5 +235,6 @@ pub fn prettify(raw: String) -> String {
     }
 
     let pretty_pinyin: String = syl_vec.join(" ");
-    return pretty_pinyin;
+
+    pretty_pinyin
 }
